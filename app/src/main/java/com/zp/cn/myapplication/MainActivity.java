1994125189e8ed229830e8029b5cd4bc20a2cfa0
Administrator <aa475v40fr>
2017-04-24 16:46:07 +0800

    完成了远程数据加载,和图片缓存操作!

--- app/src/main/java/com/zp/cn/myapplication/MainActivity.java
+++ app/src/main/java/com/zp/cn/myapplication/MainActivity.java
@@ -4,69 +4,138 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.zp.cn.myapplication.model.Student;
 
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.image.ImageOptions;
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+@ContentView(R.layout.activity_main)
 public class MainActivity extends Activity {
 
+    @ViewInject(R.id.lv)
     private ListView listView;
 
     @Override  // 在Activty创建的时候会加载布局文件
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // 获取ListView并且填充数据
-        listView = (ListView) findViewById(R.id.lv);
+        x.view().inject(this);
+//        setContentView(R.layout.activity_main);
+        // 获取ListView并且填充数据 (findViewById 当前布局文件: R.layout.activity_main)去查找
+//        listView = (ListView) this.findViewById(R.id.lv);
         // 下午数据会从服务器获取
 
-        // final 则说明在JVM 常量池,
-        final List<Student> stuList = new ArrayList<Student>();
-        for (int i = 0; i < 20; i++) {
-            stuList.add(new Student("https://ss1.bdstatic.com/5eN1bjq8AAUYm2zgoY3K/r/www/cache/holiday/habo/res/doodle/6.png",
-                    "1831234567" + i, "工资:10000"));
-        }
-        // listView.addView(); 必须要创建Adapter适配器在装载数据,否则直接addView会抛出异常
-        listView.setAdapter(new BaseAdapter() {
-            @Override   // 获取当前集合的总数量
-            public int getCount() {
-                return stuList.size();
+        // RequestParams：配置服务器的地址 需要网络权限
+        RequestParams params = new RequestParams("http://wxynr88312.jsp.fjjsp.net/result.txt");
+        // 配置ssl
+//        params.setSslSocketFactory(...); // 设置ssl
+        // 如果需要传递参数,则配置
+//        params.addQueryStringParameter("wd", "xUtils");
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override  // 如果成功,返回的是基于json的字符串
+            public void onSuccess(String result) {  //  string -->json 然后在赋值
+//                Toast.makeText(x.app(), result, Toast.LENGTH_LONG).show();
+//                Log.i("zp","result:" + result);
+                // 重新把JSON解析集合
+                Gson gson = new Gson();
+                List<Student> stuList = (List<Student>)gson.fromJson(result, new TypeToken<List<Student>>() {
+                }.getType());
+                Log.i("zp","result:" + stuList);
+//                listView.addView(); 必须要创建Adapter适配器在装载数据,否则直接addView会抛出异常
+                listView.setAdapter(new MyAdapter(stuList));
             }
 
-            @Override //  获取每个数据项目
-            public Object getItem(int position) {
-                return stuList.get(position);
+            @Override  // 请求出错,调用此方法
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Toast.makeText(x.app(), ex.getMessage(), Toast.LENGTH_LONG).show();
             }
 
-            @Override  // 获取数据项ID
-            public long getItemId(int position) {
-                Student student = stuList.get(position);
-                return 0;
+            @Override  // 请求取消测调用此方法
+            public void onCancelled(CancelledException cex) {
+                Toast.makeText(x.app(), "cancelled", Toast.LENGTH_LONG).show();
             }
 
-            @Override  // getView：获取布局文件,并且加载到ListView中
-            public View getView(int position, View convertView, ViewGroup parent) {
-                // 是在当前layout配置文件中去查找
-
-                return null;
+            @Override  // 无论成功还是失败次方法都会被调用
+            public void onFinished() {
+                Toast.makeText(x.app(), "onFinished", Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    public void goUrl(View view) {
-        EditText txt = (EditText) findViewById(R.id.edit_url);
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(txt.getText().toString()));
-        // 访问网络需要权限
-        startActivity(intent);
+    private class MyAdapter extends  BaseAdapter{
+        // 此数据在创建Adapter对象时必须传入
+        private List<Student> stuList = null;
+
+        public MyAdapter(List<Student> stuList){
+            this.stuList = stuList;
+        }
+
+        @Override   // 获取当前集合的总数量
+        public int getCount() {
+            return stuList.size();
+        }
+
+        @Override //  获取每个数据项目
+        public Object getItem(int position) {
+            return stuList.get(position);
+        }
+
+        @Override  // 获取数据项ID
+        public long getItemId(int position) {
+            Student student = stuList.get(position);
+            return 0;
+        }
+
+        @Override  // getView：获取布局文件,并且加载到ListView中
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Log.i("zp", "position:" + position + ",convertView" + convertView + ",parent:" + parent);
+            View listItem = null;
+            if (convertView != null) {  // 缓存中已有之前ListItem布局,因此不需要在重新获取(但是数据需要更新)
+                listItem = convertView;
+            } else {
+                // 是在当前layout配置文件中去查找
+                listItem = View.inflate(MainActivity.this, R.layout.list_item, null);
+            }
+            // 数据需要重新赋值
+            TextView tvTel = (TextView) listItem.findViewById(R.id.txt_tel);
+            TextView tvSalary = (TextView) listItem.findViewById(R.id.txt_salary);
+            ImageView iv = (ImageView) listItem.findViewById(R.id.iv);
+            Student student = (Student) getItem(position);
+            tvTel.setText(student.getTel());
+            tvSalary.setText(student.getSalary());
+
+            ImageOptions imageOptions = new ImageOptions.Builder()
+                    //设置加载过程中的图片
+                    .setLoadingDrawableId(R.drawable.weather)
+                    //设置加载失败后的图片
+                    .setFailureDrawableId(R.drawable.weather)
+                    //设置使用缓存
+                    .setUseMemCache(true)
+                    //设置支持gif
+                    .setIgnoreGif(false)
+                    .build();
+            x.image().bind(iv,student.getImgUrl(),imageOptions);
+            return listItem;
+        }
     }
 }
---
+++ app/src/main/java/com/zp/cn/myapplication/utils/MyApplication.java
@@ -0,0 +1,23 @@
+package com.zp.cn.myapplication.utils;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.zp.cn.myapplication.BuildConfig;
+
+import org.xutils.x;
+
+/**
+ * 此处用来对项目启动时的初始化操作
+ */
+
+public class MyApplication extends Application {
+    @Override  // 当application创建的时候此方法被调用
+    public void onCreate() {
+        super.onCreate();
+        // 1: 初始化配置, 2：获取远程的数据  3：对框架进行初始化
+        Log.i("zp","MyApplication onCreate");
+        x.Ext.init(this);
+        x.Ext.setDebug(BuildConfig.DEBUG); // 是否输出debug日志, 开启debug会影响性能.
+    }
+}
