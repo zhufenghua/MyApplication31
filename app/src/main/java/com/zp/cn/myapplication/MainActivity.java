bc30110995bbc250e2f820e6165ad3c0e32aaf1b
Administrator <aa475v40fr>
2017-04-24 09:32:40 +0800

    完成了访问互联网的功能

--- app/src/main/java/com/zp/cn/myapplication/MainActivity.java
+++ app/src/main/java/com/zp/cn/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.zp.cn.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goUrl(View view){
+        EditText txt =   (EditText)findViewById(R.id.edit_url);
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(txt.getText().toString()));
+        // 访问网络需要权限
+        startActivity(intent);
     }
 }
